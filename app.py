639a407eca3bc406dfd03143e5a95eac8a8f100e
Aimlclubcmrit <98299719+Aimlclubcmrit@users.noreply.github.com>
2022-03-17 01:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import render_template,url_for,Flask
 app=Flask(__name__,static_url_path='/static')
-app.route("/")
+@app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
